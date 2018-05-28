4044bf9fc5c9cc5f83289016937138e8fd6c47b6
Cristian Silva Grosseli <cristian23sai@gmail.com>
2018-05-28 16:27:15 -0300

    Initial commit

---
+++ app/src/main/java/cristian/genius/SequenceManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequenceManager {
+
+    private List listOfSteps;
+
+    public SequenceManager() {
+        listOfSteps = new ArrayList();
+    }
+
+}

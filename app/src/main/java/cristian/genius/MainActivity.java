2b6d137aa6bedab03867a5e07f2781bb77c5f2a6
Cristian Silva Grosseli <cristian23sai@gmail.com>
2018-06-15 18:15:52 -0300

    created Genius interface Activity

--- app/src/main/java/cristian/genius/MainActivity.java
+++ app/src/main/java/cristian/genius/MainActivity.java
@@ -1,7 +1,9 @@
 package cristian.genius;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startGameButton = (Button) findViewById(R.id.startGameButton);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GeniusActivity.class);
+            }
+        });
     }
 }
---
+++ app/src/main/java/cristian/genius/activity_genius.java
@@ -0,0 +1,15 @@
+package cristian.genius;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_genius extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genius);
+
+
+    }
+}
